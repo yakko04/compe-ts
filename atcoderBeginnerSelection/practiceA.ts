6ddb047d7b6d853e69069919850c443ec857f53f
yakko04 <yakyukozo2001@gmail.com>
2025-10-30 21:57:56 +0900

    solve practiceA

---
+++ atcoderBeginnerSelection/practiceA.ts
@@ -0,0 +1,12 @@
+import { readFileSync } from 'node:fs';
+
+function Main(input: string) {
+    const lines = input.split('\n');
+    const a = parseInt(lines[0]);
+    const lines2 = lines[1].split(' ');
+    const b = parseInt(lines2[0]);
+    const c = parseInt(lines2[1]);
+    console.log("%d %s", a + b + c, lines[2]);
+}
+
+Main(readFileSync('/dev/stdin', 'utf8'));
